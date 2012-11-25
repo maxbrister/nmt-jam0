4b5ae0f34ffd096df887d5defe4470b4f5c7f649
Benjamin Bean <benjamin@poslavu.com>
2012-11-25 07:55:56 -0500

    now with coding conventions

--- entity.py
+++ entity.py
@@ -1,10 +1,10 @@
 class Entity:
-    SpriteStill = None
-    SpriteMoveUp = None
-    SpriteMoveDown = None
-    SpriteMoveLeft = None
-    SpriteMoveRight = None
-    Position = None
+    __SpriteStill = None
+    __SpriteMoveUp = None
+    __SpriteMoveDown = None
+    __SpriteMoveLeft = None
+    __SpriteMoveRight = None
+    __Position = None
 
     """
     " @SpriteStill the basic sprite, if no other sprites are defined this one is used
@@ -15,15 +15,17 @@ class Entity:
     "     SpriteMoveLeft defaults to this if not defined
     " @SpriteMoveDown/SpriteMoveLeft should be self explanatory
     """
-    def __init__(self, SpriteStill, Position, SpriteMoveUp=None, SpriteMoveRight=None, SpriteMoveDown=None, SpriteMoveLeft=None):
+    def __init__(self, spriteStill, position, spriteMoveUp=None, spriteMoveRight=None, spriteMoveDown=None, spriteMoveLeft=None):
         pass
     
-    def interact(self, Entity):
+    def Interact(self, entity):
         pass
     
     """
     " Tells an entity to move one space toward the given direction
     " Can be 0, 1, 2, 3 or can be "up", "right", "down", "left"
     """
-    def move(self, Direction):
+    def Move(self, direction):
         pass
+
+    
