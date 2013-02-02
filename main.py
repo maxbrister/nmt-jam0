f831a26c379e4442850241efb147e673ab86b2a1
Max Brister <max@2bass.com>
2013-02-01 20:08:36 -0700

    main.py: Add cairo test

--- main.py
+++ main.py
@@ -2,12 +2,39 @@
 import sys
 import pygame
 import pygame.locals
+import cairo
+import numpy
+import math
+import Image
 
 class Window(object):
     def __init__(self, name = 'Hobo Sim 2013', size=(800,600)):
+        # initialize pygame
         pygame.init()
         self._window = pygame.display.set_mode(size)
         pygame.display.set_caption(name)
+
+        # data to talk between pygame and cairo
+        self._data = numpy.empty(size[0] * size[1] * 4, dtype=numpy.int8)
+
+        # initialize cairo
+        self._surface = cairo.ImageSurface.create_for_data(self._data, cairo.FORMAT_ARGB32, size[0], size[1], size[0] * 4)
+
+        # test rendering
+        ctx = cairo.Context(self._surface)
+        ctx.set_line_width(15)
+        ctx.arc(320, 240, 200, 0, 2 * math.pi)
+        ctx.set_source_rgba(.6,0,.4,1)
+        ctx.fill_preserve()
+        ctx.set_source_rgba(0,.84,.2,.5)
+        ctx.stroke()
+        img = Image.frombuffer('RGBA', (self._surface.get_width(),
+                                        self._surface.get_height()),
+                               self._surface.get_data(), 'raw', 'BGRA', 0, 1)
+        img = img.tostring('raw', 'RGBA', 0, 1)
+        psurf = pygame.image.frombuffer(img, size, 'RGBA')
+        self._window.blit(psurf, (0, 0))
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
