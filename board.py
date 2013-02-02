5033946348071a245d676e64660c7f93076208fe
Max Brister <max@2bass.com>
2013-02-02 04:07:04 -0700

    Fix call to GetEntity in Board.Movable

--- board.py
+++ board.py
@@ -43,7 +43,7 @@ class Board(object):
                     (position[0]+1, position[1]),
                     (position[0]  , position[1]+1),
                     (position[0]  , position[1]-1)]:
-            if self.InRange(pos) and GetEntity(pos) is None:
+            if self.InRange(pos) and self.GetEntity(pos) is None:
                 ret.add(pos)
         
         return ret
