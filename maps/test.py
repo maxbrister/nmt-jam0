6c434f470e45768fd27e3fe56febc1c5d53b11a8
Andre Infante <ainfante@nmt.edu>
2013-02-03 03:46:16 -0600

    added the friendly old hobo entity

--- maps/test.py
+++ maps/test.py
@@ -1,5 +1,5 @@
 def Initialize(entity, board):
-    ent0 = entity.NPC('hobo', (6, 6), board)
+    ent0 = entity.NPC('blind', (6, 6), board)
     ent0.AddToDialogueList('foo', 'Change for a dying man?')
     ent0.AddToDialogueList('foo', 'Oh.  I see.  Nevermind.')
     ent0.AddToDialogueList('foo', 'I am an old man, and do not have long left.  Will you listen?')
@@ -8,5 +8,5 @@ def Initialize(entity, board):
 
 
     
-    ent1 = entity.NPC('beer', [(2, 3), (6, 3), (6, 6), (2, 6)], board)
+    ent1 = entity.NPC('beer', [(2, 3), (4, 3), (4, 4), (2, 4)], board)
     ent1.AddToDialogueList('foo', 'You should really take your medication.')
