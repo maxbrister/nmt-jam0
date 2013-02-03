52560f11f7e5ad72a47fb92f37c52bc21c04ec17
Benjamin Bean <bbean@nmt.edu>
2013-02-02 21:25:25 -0700

    attacks between creatures are mostly working

--- creature.py
+++ creature.py
@@ -8,11 +8,11 @@ class State(object):
     #     eg lambda z : (random() < 0.5)
     def __init__(self, name, passiveChanges=None, disableAttack=None):
         self._name = name
-        self._passiveChange = passiveChange
+        self._passiveChanges = passiveChanges
         self._disableAttack = disableAttack
         
     def Update(self, creature):
-        if (self._passiveChange==None):
+        if (self._passiveChanges==None):
             return
         for pc in self._passiveChanges:
             creature._currentStats[pc[0]] += pc[1]
@@ -23,89 +23,94 @@ class State(object):
         else:
             return self._disableAttack(lambdaInput)
         
-# attributes ["speed", "damage", "drunkeness", "levelingRate"]
+# attributes ["speed", "attack", "drunkeness", "defense", "levelingRate"]
 POSSIBLE_STATES = {
-    "normal": State("normal"),
     "poisoned": State("poisoned",((2,-1)) ),
     "sleeping": State("sleeping",None,True),
     "frightened": State("frightened",None,True)
     }
 
 class Attack(object):
-    # stateToAdd should be a State object
+    # statesToAdd should be a State object or tuple of state objects
     # statChanges should be a tuple of stats to change
     #     eg ((0,1), (2,-1))
-    def __init__(self, name, damage, recoil = 0, stateToAdd=None, stateChangeProbability = 0.2, critsAgainst = (), statChanges=None, statChangeProbability = 0):
-        self._damage = damage
+    def __init__(self, name, attack, recoil = 0, statesToAdd=None, stateChangeProbability = 0.2, critsAgainst = (), statChanges=None, statChangeProbability = 0):
+        self._attack = attack
         self._recoil = recoil
-        self._stateToAdd = stateToAdd
-        self._statChangeProbability = statChangeProbability
-        self._critsAgainst = critsAgainst
-        self._stateChange = stateChange
+        self._statesToAdd = statesToAdd
         self._stateChangeProbability = stateChangeProbability
+        self._critsAgainst = critsAgainst
+        self._statChanges = statChanges
+        self._statChangeProbability = statChangeProbability
         
     def Attack(self, attacker, deffender):
-        attacker.SetStat("drunkeness", attacker.GetStat("drunkeness")-self._recoil*attacker.GetStat("damage"))
+        # attributes ["speed", "attack", "drunkeness", "defense", "levelingRate"]
+        attacker.SetStat(2, attacker._currentStats[2]-self._recoil*attacker._currentStats[1])
         critDamage = 1.0
         if (random() > 0.95):
             critDamage *= 2.0
-        if (deffender.GetName in self._critsAgainst):
+        if (deffender._name in self._critsAgainst):
             critDamage *= 2.0
-        deffender.setStat("drunkeness", deffender.GetStat("drunkeness")-self._damage*attacker.GetStat("damage")*critDamage)
-        for statChange in self._statChange:
-            if (random() < self._statChangeProbability):
-                deffender.ModifyStat(statChange[0], statChange[1])
-        for stateChange in self._stateChange:
-            if (random() < self._stateChangeProbability):
-                deffender.AddState(stateChange)
-
+        # attributes ["speed", "attack", "drunkeness", "defense", "levelingRate"]
+        deffender.SetStat(2, deffender._currentStats[2]-self._attack*attacker._currentStats[1]*critDamage/deffender._currentStats[3])
+        if (self._statChanges):
+            for statChange in self._statChanges:
+                if (random() < self._statChangeProbability):
+                    deffender.ModifyStat(statChange[0], statChange[1])
+        if (self._statesToAdd):
+            for stateChange in self._statesToAdd:
+                if (random() < self._stateChangeProbability):
+                    deffender.AddState(stateChange)
+                
+    def __repr__(self):
+        return ["attack "+self._attack.__repr__(), "recoil "+self._recoil.__repr__(), "states "+self._statesToAdd.__repr__(), "probability "+self._stateChangeProbability.__repr__(), "crits "+self._critsAgainst.__repr__(), "stats "+self._statChanges.__repr__(), "probability "+self._statChangeProbability.__repr__()].__repr__();
+    
 ATTACKS = {
     "PassiveAgressiveStickyNotes": Attack("Passive Agressive Sticky Notes",2.0),
     "InsultYourCode": Attack("Insult Your Code",1.5,critsAgainst=("Programmer")),
     "UseStarTrekTrivia": Attack("Use Star Trek Trivia",1.0,0,POSSIBLE_STATES["sleeping"],0.6),
     "KickShins": Attack("Kick Shins",3.0,0.5,critsAgainst=("Dog")),
     "Bite": Attack("Bite",3.0),
-    "Chew Bones": Attack("Chew Bones",2.0,critsAgainst=("Dog")),
-    "Growl Menacingly": Attack("Growl Menacingly",0,0,POSSIBLE_STATES["frightened"],0.4),
-    "Eat Shoes": Attack("Eat Shoes",2.0,critsAgains=("Programmer"))
+    "ChewBones": Attack("Chew Bones",2.0,critsAgainst=("Dog")),
+    "GrowlMenacingly": Attack("Growl Menacingly",0,0,POSSIBLE_STATES["frightened"],0.4),
+    "EatShoes": Attack("Eat Shoes",2.0,critsAgainst=("Programmer"))
     }
 
-# attributes ["speed", "damage", "drunkeness", "levelingRate"]
+# attributes ["speed", "attack", "drunkeness", "defense", "levelingRate"]
 CREATURES = {
-    "Programmer": {"attributes": (2.0, 0.5, 8.0, 1.4),
+    "Programmer": {"attributes": [2.0, 0.5, 8.0, 1.2, 1.4],
                    "stateRecoveryRate": 2,
                    "attacks": (
-            ATTACKS("PassiveAgressiveStickyNotes"),
-            ATTACKS("InsultYourCode"),
-            ATTACKS("UseStarTrekTrivia"),
-            ATTACKS("KickShins")
+            ATTACKS["PassiveAgressiveStickyNotes"],
+            ATTACKS["InsultYourCode"],
+            ATTACKS["UseStarTrekTrivia"],
+            ATTACKS["KickShins"]
             ),
-                   "attackLevels": (0, 0, 0, 0) },
-    "Dog": {"attributes": (3.0, 2.0, 5.0, 1.1),
+                   "attackLevels": [0, 0, 0, 0] },
+    "Dog": {"attributes": [3.0, 2.0, 5.0, 1.1, 1.1],
             "stateRecoveryRate": 3,
             "attacks": (
-            Attack("Bite"),
-            Attack("ChewBones"),
-            Attack("GrowlMenacingly"),
-            Attack("EatShoes")
+            ATTACKS["Bite"],
+            ATTACKS["ChewBones"],
+            ATTACKS["GrowlMenacingly"],
+            ATTACKS["EatShoes"]
             ),
-            "attackLevels": (0, 0, 0, 0) }
+            "attackLevels": [0, 0, 0, 0] }
     }
 
 class Creature(object):
     def __init__(self, creatureType):
-        # each action is a tuple (action name, damage multiplier)
+        self._level = 0
         self._attributes = CREATURES[creatureType]["attributes"]
         self._currentStats = self._attributes
         self._name = creatureType
-        self._state = set(["normal"])
+        self._state = []
         self._stateRecoveryStages = {"normal":0,"poisoned":0,"sleeping":0,"frightened":0}
         self._stateRecoveryRate = CREATURES[creatureType]["stateRecoveryRate"]
         self._allAttacks = CREATURES[creatureType]["attacks"]
-        self._attackLevels = CREATURETYPE[creatureType]["attackLevels"]
-        self._attacks = None
+        self._attackLevels = CREATURES[creatureType]["attackLevels"]
+        self._attacks = []
         self._autoAssignAttacks()
-        self._level = 0
 
     """
     " adds the latest four attacks that the creature knows to the list of attacks
@@ -114,7 +119,7 @@ class Creature(object):
         for x in range(len(self._allAttacks)):
             i = -(x+1)
             if (self._level >= self._attackLevels[i]):
-                self._attacks.add(self._allAttacks[x])
+                self._attacks.append(self._allAttacks[x])
                 if (len(self._attacks) == 4):
                     return
 
@@ -134,7 +139,9 @@ class Creature(object):
         self._currentStats[statIndex] += value
 
     def AddState(self, state):
-        self._state.add(state)
+        if (state in self._state):
+            return
+        self._state.append(state)
         self._stateRecoveryStages[state._name] = 10
         
     """
@@ -143,11 +150,11 @@ class Creature(object):
     " returns False otherwise
     """
     def UpdateState(self):
-        removeFrom = set()
+        removeFrom = []
         for state in self._states:
             self._stateRecoveryStages[stateName] = max(0, self._stateRecoveryStages[stateName]-self._stateRecoveryRate)
             if (self._stateRecoveryStages[stateName] == 0):
-                removeFrom.add(state)
+                removeFrom.append(state)
             # check the states, specifically
             state.Update(self)
         for remove in removeFrom:
@@ -156,6 +163,13 @@ class Creature(object):
             self._die()
             return True
         return False
+                
+    def __repr__(self):
+        return ["name "+self._name, "level "+self._level.__repr__(), "stats "+self._currentStats.__repr__(), "max attrs "+self._attributes.__repr__(), "states "+self._state.__repr__(), "recovery rates "+self._stateRecoveryRate.__repr__(), "attacks "+self._attacks.__repr__()].__repr__()
 
 if __name__ == "__main__":
     c = Creature("Programmer")
+    print c
+    c.Attack(c._attacks[0], c)
+    print ""
+    print c
